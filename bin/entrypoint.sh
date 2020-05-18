c8161da3de2cd97d89a7d02872ff5bf03cb1e737
Flore de Lasteyrie <flore.delasteyrie@gmail.com>
2020-05-18 20:53:39 +0200

    feat: add cz config if not set

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,6 +5,28 @@ if [ -n "$GITHUB_WORKSPACE" ]; then
     echo "Switching to Github Workspace: ${GITHUB_WORKSPACE}"
 fi
 
+PYPROJECT=pyproject.toml
+PYPROJECT_CZ_CONFIG="[tool.commitizen]"
+PYPROJECT_CONFIG=$"\n\
+[tool.commitizen]\n\
+name = 'cz_conventional_commits'\n\
+version = '0.1.0'\n\
+tag_format = '\$version'\n\
+bump_message = 'chore: bump version \$current_version → \$new_version'\n\
+version_files = [\n\
+  'pyproject.toml:version'\n\
+]"
+
+PACKAGE_JSON=package.json
+
+if test -f "$PYPROJECT"; then
+    if ! grep -Fxq "$PYPROJECT_CZ_CONFIG" "$PYPROJECT"; then
+        echo -e "$PYPROJECT_CONFIG" >> "$PYPROJECT"
+    fi
+elif test -f "$PACKAGE_JSON"; then
+    echo To be implemented
+fi
+
 git fetch --tags
 cz bump --no-verify --yes
 if [ $? -ne 0 ]; then
