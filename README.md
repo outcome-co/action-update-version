0d802b6039215c453162c4155f1446282f2fa333
Flore de Lasteyrie <flore.delasteyrie@gmail.com>
2020-05-19 13:45:07 +0200

    chore: clean repo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # action-update-version
 ![ci-badge-checks](https://github.com/outcome-co/action-update-version/workflows/Checks/badge.svg) ![ci-badge-release](https://github.com/outcome-co/action-update-version/workflows/Release/badge.svg)
 
-This Github Action will automatically version your repositories.
+This Github Action automatically updates the versions of your repositories.
 
 ## Description
 
--- bin/cz_init.sh
+++ bin/cz_init.sh
@@ -5,23 +5,23 @@ PACKAGE_JSON=package.json
 DEFAULT_VERSION=VERSION
 
 CZ_CONFIG="[tool.commitizen]"
-BASE_CONFIG=$"\n\
+BASE_CONFIG="\n\
 [tool.commitizen]\n\
 name = 'cz_conventional_commits'\n\
 version = '0.1.0'\n\
 tag_format = '\$version'"
 
-MESSAGE_CONFIG=$"bump_message = 'chore: bump version \$current_version → \$new_version'"
-VERSION_FILE_PYPROJECT=$"version_files = ['pyproject.toml:version']"
-VERSION_FILE_NPM=$"version_files = ['package.json:version']"
-VERSION_FILE_OTHER=$"version_files = ['VERSION']"
+MESSAGE_CONFIG="bump_message = 'chore: bump version \$current_version → \$new_version'"
+VERSION_FILE_PYPROJECT="version_files = ['pyproject.toml:version']"
+VERSION_FILE_NPM="version_files = ['package.json:version']"
+VERSION_FILE_OTHER="version_files = ['VERSION']"
 
 if test -f "$PYPROJECT" && ! grep -Fxq "$CZ_CONFIG" "$PYPROJECT"; then
     echo "$BASE_CONFIG">> "$PYPROJECT"
     echo "$MESSAGE_CONFIG">> "$PYPROJECT"
     echo "$VERSION_FILE_PYPROJECT">> "$PYPROJECT"
 
-elif ! test -f "$CZ_TOML"; then
+elif ! test -f "$PYPROJECT" && ! test -f "$CZ_TOML"; then
     echo "$BASE_CONFIG">> "$CZ_TOML"
     echo "$MESSAGE_CONFIG">> "$CZ_TOML"
 
