9cee5711063f6049951bd9f681a58069699187e8
Flore de Lasteyrie <flore.delasteyrie@gmail.com>
2020-05-20 17:23:18 +0200

    docs: add explanations for entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,8 +5,14 @@ if [ -n "$GITHUB_WORKSPACE" ]; then
     echo "Switching to Github Workspace: ${GITHUB_WORKSPACE}"
 fi
 
+# Get tags from repo
 git fetch --tags
+
+# Bump version with Commitizen: checks git log messages since last tag to determine new version
+# Then commits a bump version message locally, and tags the new version locally
 cz bump --no-verify --yes
+
+# The syntax "::set-output name=[key]::[value]" allows to set outputs inside the Github Workflow
 if [ $? -ne 0 ]; then
 echo ::set-output name=updated::false
 else
