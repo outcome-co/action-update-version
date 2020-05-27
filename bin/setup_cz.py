6c143a7544c640ffd1aa90aa8ce35af95912aa64
Douglas Willcocks <douglas@d-tw.org>
2020-05-27 18:32:32 +0200

    refactor: :art: refactor init script

--- bin/setup_cz.py
+++ bin/setup_cz.py
@@ -1,97 +1,115 @@
-"""Setup Commitizen."""
+"""Set up Commitizen.
+
+If there is a pyproject.toml file in the directory, we'll use that to store the cz config,
+otherwise we'll use .cz.toml.
+
+The script will detect the version files to update, either pyproject.toml, package.json, or VERSION.
+"""
 
 import json
 import logging
-import os
+from pathlib import Path
 import sys
 
 import toml
 
-cz_toml = '.cz.toml'
-pyproject = 'pyproject.toml'
+cz_toml = Path('.cz.toml')
+pyproject = Path('pyproject.toml')
+package_json = Path('package.json')
+version_other = Path('VERSION')
 
-version_node = 'package.json'
-version_other = 'VERSION'
 default_version = '0.1.0'
 
 
-def setup_cz():
-    if is_cz_config_needed(pyproject):
-        add_cz_config(pyproject)
-    elif is_cz_config_needed(cz_toml):
-        add_cz_config(cz_toml)
+def main():
+    if pyproject.exists():
+        if not is_cz_in_toml(pyproject):
+            add_cz_config(pyproject)
+        else:
+            logging.info(f'cz already configured in {pyproject}')
+
+    elif cz_toml.exists():
+        if not is_cz_in_toml(cz_toml):
+            logging.error(f'{cz_toml} exists, but does not contain valid config!')
+            sys.exit(-1)
+
+        logging.info(f'cz already configured in {cz_toml}')
+
     else:
         add_cz_config(cz_toml)
 
 
-def is_cz_config_needed(toml_file):
-    if os.path.exists(toml_file):
-        logging.info(f'{toml_file} exists')
-
-        if is_cz_in_toml(toml_file):
-            logging.info(f'{toml_file} already configured with Commitizen')
-            sys.exit()
-
-        logging.info(f'{toml_file} not configured with Commitizen')
-        return True
-
-    logging.info(f'{toml_file} not found')
-    return False
-
-
 def is_cz_in_toml(toml_file):
+    """Check if the TOML file contains CZ config.
+
+    Args:
+        toml_file (Path): The TOML file to check.
+
+    Returns:
+        bool: True if the file contains CZ config
+    """
     parsed_toml = toml.load(toml_file)
-    if 'commitizen' in parsed_toml['tool'].keys():
-        return True
-    return False
+    return 'commitizen' in parsed_toml['tool'].keys()
 
 
 def add_cz_config(toml_file):
-    version, version_file = get_version_and_filepath(toml_file)
+    logging.info(f'Adding Commitizen config to {toml_file}')
+
+    version, version_file = get_current_version_info()
     logging.info(f'Version set to {version} with filepath {version_file}')
 
     cz_config = cz_config_template(version, version_file)
-    with open(toml_file, 'a') as filename:
-        filename.write(cz_config)
-    logging.info(f'Commitizen config added to {toml_file}')
+
+    existing_config = {}
+
+    if toml_file.exists():
+        with open(toml_file, 'r') as toml_file_handle:
+            existing_config = toml.load(toml_file_handle)
+
+    config = {**existing_config, **cz_config}
+
+    with open(toml_file, 'w') as toml_file_handle:
+        toml.dump(config, toml_file_handle)
 
 
-def get_version_and_filepath(toml_file):
-    if toml_file == pyproject:
-        parsed_toml = toml.load(toml_file)
-        return parsed_toml['tool']['poetry']['version'], [f'{toml_file}:version']
+def get_current_version_info():
+    """Determines the version file and current version.
 
-    elif os.path.exists(version_node):
-        parsed_json = json.load(open(version_node))  # noqa: WPS515
-        return parsed_json['version'], [f'{version_node}:version']
+    Returns:
+        Tuple[str, Path]: The current version number and the version file.
+    """
+    if pyproject.exists():
+        parsed_toml = toml.load(pyproject)
+        return parsed_toml['tool']['poetry']['version'], f'{pyproject}:version'
 
-    create_version_other_if_needed()
-    file = open(version_other, 'r')  # noqa: WPS515
-    return file.readline(), [version_other]
+    if package_json.exists():
+        parsed_json = json.load(open(package_json))  # noqa: WPS515
+        return parsed_json['version'], f'{package_json}:version'
 
+    if version_other.exists():
+        version = open(version_other, 'r').read().strip()  # noqa: WPS515
+        return version, version_other
 
-def create_version_other_if_needed():
-    if not os.path.exists(version_other):
-        with open(version_other, 'w') as file:
-            file.write(default_version)
+    with open(version_other, 'w') as version_other_handle:
+        version_other_handle.write(f'{default_version}\n')
+
+    return default_version, version_other
 
 
 def cz_config_template(version, version_file):
-    cz_config = {
+    return {
         'tool': {
             'commitizen': {
                 'name': 'cz_conventional_commits',
                 'version': version,
                 'tag_format': 'v$version',
                 'bump_message': 'chore(version): $current_version → $new_version',
-                'version_files': version_file,
+                'version_files': [version_file],
             },
         },
     }
-    cz_config_formatted = toml.dumps(cz_config)
-    return '\n{0}'.format(cz_config_formatted)
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    setup_cz()
+    main()
