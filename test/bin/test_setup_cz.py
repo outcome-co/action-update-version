a21c58ff005ced44db69bb02b1c95660ecbf60ee
Flore de Lasteyrie <flore.delasteyrie@gmail.com>
2020-05-29 00:07:24 +0200

    test: main and if cz in toml

--- test/bin/test_setup_cz.py
+++ test/bin/test_setup_cz.py
@@ -2,30 +2,68 @@
 
 from unittest.mock import patch
 
+import pytest
+
 from bin import setup_cz
 
 
-class TestSetupCz:
+class TestMain:
+    @patch('pathlib.Path.exists', return_value=True, autospec=True)
+    @patch('bin.setup_cz.is_cz_in_toml', return_value=True, autospec=True)
     @patch('bin.setup_cz.add_cz_config', autospec=True)
-    @patch('bin.setup_cz.is_cz_config_needed', return_value=True, autospec=True)
-    def test_setup_pyproject(self, mock_is_cz_config_needed, mock_add_cz_config):
-        setup_cz.setup_cz()
+    def test_pyproject_exist_and_configured(self, mock_add_cz_config, mock_is_cz_in_toml, mock_path_exists):
+        setup_cz.main()
+        mock_add_cz_config.assert_not_called()
+
+    @patch('pathlib.Path.exists', return_value=True, autospec=True)
+    @patch('bin.setup_cz.is_cz_in_toml', return_value=False, autospec=True)
+    @patch('bin.setup_cz.add_cz_config', autospec=True)
+    def test_pyproject_exist_and_not_configured(self, mock_add_cz_config, mock_is_cz_in_toml, mock_path_exists):
+        setup_cz.main()
         mock_add_cz_config.assert_called_with(setup_cz.pyproject)
 
-    @patch('bin.setup_cz.add_cz_config', autospec=True)
-    @patch('bin.setup_cz.is_cz_config_needed', autospec=True)
-    def test_setup_cz_toml(self, mock_is_cz_config_needed, mock_add_cz_config):
+    @patch('pathlib.Path.exists', autospec=True)
+    @patch('bin.setup_cz.is_cz_in_toml', return_value=True, autospec=True)
+    @patch('logging.info', autospec=True)
+    def test_cz_toml_exist_and_configured(self, mock_logging_info, mock_is_cz_in_toml, mock_path_exists):
         def side_effect(arg):  # noqa: WPS430
             if arg == setup_cz.pyproject:
                 return False
             return True
 
-        mock_is_cz_config_needed.side_effect = side_effect
-        setup_cz.setup_cz()
+        mock_path_exists.side_effect = side_effect
+        setup_cz.main()
+        mock_logging_info.assert_called()
+
+    @patch('pathlib.Path.exists', autospec=True)
+    @patch('bin.setup_cz.is_cz_in_toml', return_value=False, autospec=True)
+    @patch('logging.error', autospec=True)
+    def test_cz_toml_exist_and_not_configured(self, mock_logging_error, mock_is_cz_in_toml, mock_path_exists):
+        def side_effect(arg):  # noqa: WPS430
+            if arg == setup_cz.pyproject:
+                return False
+            return True
+
+        mock_path_exists.side_effect = side_effect
+        with pytest.raises(SystemExit):
+            setup_cz.main()
+            mock_logging_error.assert_called()
+
+    @patch('pathlib.Path.exists', return_value=False, autospec=True)
+    @patch('bin.setup_cz.add_cz_config', autospec=True)
+    def test_else(self, mock_add_cz_config, mock_path_exists):
+        setup_cz.main()
         mock_add_cz_config.assert_called_with(setup_cz.cz_toml)
 
-    @patch('bin.setup_cz.add_cz_config', autospec=True)
-    @patch('bin.setup_cz.is_cz_config_needed', return_value=False, autospec=True)
-    def test_setup_other(self, mock_is_cz_config_needed, mock_add_cz_config):
-        setup_cz.setup_cz()
-        mock_add_cz_config.assert_called_with(setup_cz.cz_toml)
+
+class TestCzInToml:
+    test_toml_file_configured = {'tool': {'commitizen': {'name': 'test'}}}
+    test_toml_file_not_configured = {'tool': {'poetry': {'name': 'test'}}}
+
+    @patch('toml.load', return_value=test_toml_file_configured, autospec=True)
+    def test_cz_is_in_toml(self, mock_toml_load):
+        assert setup_cz.is_cz_in_toml('toml_file_configured') is True
+
+    @patch('toml.load', return_value=test_toml_file_not_configured, autospec=True)
+    def test_cz_is_not_in_toml(self, mock_toml_load):
+        assert setup_cz.is_cz_in_toml('toml_file_not_configured') is False
